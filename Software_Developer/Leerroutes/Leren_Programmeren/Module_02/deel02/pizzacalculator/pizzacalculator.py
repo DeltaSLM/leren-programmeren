cc5d96ac062409426346a73d171c9ae5b890e8c1
MiataBoy <miataboymx@gmail.com>
2022-09-15 13:09:47 +0200

    Remove commented parts

--- Software_Developer/Leerroutes/Leren_Programmeren/Module_02/deel02/pizzacalculator/pizzacalculator.py
+++ Software_Developer/Leerroutes/Leren_Programmeren/Module_02/deel02/pizzacalculator/pizzacalculator.py
@@ -2,29 +2,6 @@
 # Pizza Calculator
 import datetime
 
-#small_prize = 7.99
-#medium_prize = 9.99
-#large_prize = 12.99
-
-#order = []
-
-#while True:
-    #pizza_choice = input("What size pizza would you like? You can choose between small, medium and large.").lower()
-
-    #def function(price):
-        #amount = int(input(f"How many {pizza_choice} pizzas would you like?"))
-        #print(f"Your order of {amount} {pizza_choice} pizzas will be ready shortly.")
-
-    #if pizza_choice == 'small':
-        #function(small_prize)
-    #if pizza_choice == 'medium':
-        #function(medium_prize)
-    #if pizza_choice == 'large':
-        #function(large_prize)
-    #if pizza_choice == 'done':
-        #current_time = datetime.datetime.now()
-        #total_price = amount * price
-
 prices = {"small":1, "medium":2, "large":10} # Prices for the pizzas
 selected = {x.lower() : 0 for x in input("What pizzas would you like? Small, Medium, large (Separated by ',')").replace(" ", '').split(",") if x.lower() in prices.keys()} # Ask for what pizzas they want, split prices with commas and remove spaces if provided
 orders = {} # The order list
@@ -51,6 +28,3 @@ print(f"""
       €{sum([prices[s]*c for (s, c) in selected.items()])}
 ---------------
 """)
-
-
-#        {current_time.strftime("%d-%m-%y")}
