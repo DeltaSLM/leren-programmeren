ca092ae0b353e28c1633776136c585d18a37e251
DeltaSLM <joshuaslui0203@gmail.com>
2022-09-22 10:12:29 +0200

    ZenFone

--- Software_Developer/Leerroutes/Challenges/smartphone.py
+++ Software_Developer/Leerroutes/Challenges/smartphone.py
@@ -1,9 +1,10 @@
 iPhone = float(input("Hoeveel kost de Iphone 13?"))
 Samsung = float(input("Hoeveel kost de Samsung Galaxy S22?"))
+zenFone = float(input("Hoeveel kost de Asus Zenfone 9?"))
 
 # Kijk of de iPhone en Samsung beide 900 of over zijn, en sluit het programma af met het advies niet een telefoon te kopen.
 
-if iPhone >= 900 and Samsung >= 900:
+if iPhone >= 900 and Samsung >= 900 and zenFone >= 900:
     if iPhone > Samsung:
         print(f"De Iphone is het duurst, de telefoon kost: {iPhone} Euro\nDe Samsung is het goedkoopst, de telefoon kost: {Samsung} Euro")
     else:
@@ -11,8 +12,20 @@ if iPhone >= 900 and Samsung >= 900:
     print("Het advies is dus geen telefoon te kopen, ze zijn te duur.")
     exit()
 
+zenMinSamsung = Samsung - zenFone
+zenMinIphone = iPhone - zenFone
+
+if zenMinSamsung >= 100 and zenMinIphone >= 100:
+    if iPhone > Samsung:
+        print(f"De iPhone 13 is het duurst, de telefoon kost: {iPhone} Euro\nDe ZenFone is het goedkoopst, de telefoon kost: {zenFone} euro\nDe Samsung Galaxy S22 zit er tussenin met: {Samsung} Euro")
+    else:
+        print(f"De Samsung Galaxy S22 is het duurst, de telefoon kost: {Samsung} Euro\nDe ZenFone is het goedkoopst, de telefoon kost: {zenFone} euro\nDe iPhone 13 zit er tussenin met: {iPhone} Euro")
+    print(f"het advies is dus de Asus ZenFone 9 te kopen. Deze is namelijk {zenMinIphone} euro goedkoper dan de iPhone 13 en {zenMinSamsung} euro goedkoper dan de Samsung Galaxy S22")
+    exit()
+
 # Kijk of de iPhone duurder is dan de Samsung.
 # Indien hij dat is, zal hij kijken of dat de iPhone 50 plus euro duurder is, in welk geval hij zal aanraden om de Samsung te kopen
+
 
 if iPhone > Samsung:
     print(f"De iPhone 13 is het duurst, de telefoon kost: {iPhone} Euro\nDe Samsung Galaxy S22 is het goedkoopst, de telefoon kost: {Samsung} Euro")
