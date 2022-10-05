2f3a77766094e4b15de03586a612d82daf330b28
DeltaSLM <joshuaslui0203@gmail.com>
2022-10-05 12:14:13 +0200

    Remove useless comments

--- Software_Developer/Leerroutes/Leren_Programmeren/Module_02/deel03/Opdracht_3/Sollicitatie.py
+++ Software_Developer/Leerroutes/Leren_Programmeren/Module_02/deel03/Opdracht_3/Sollicitatie.py
@@ -1,24 +1,3 @@
-# Stel eerst alle vragen en ga daarna pas beoordelen aan de hand van de gegeven antwoorden of de kandidaat mag solliciteren.
-#
-# De kandidaat moet wel voldoen aan de volgende criteria:
-#
-#     Meer dan 4 jaar praktijkervaring met dieren-dressuur OF meer dan 5 jaar ervaring met jongleren OF meer dan 3 jaar praktijkervaring met acrobatiek
-#     In bezit van een Diploma MBO-4 ondernemen
-#     In bezit van een geldig Vrachtwagen rijbewijs
-#     In bezit van een hoge hoed
-#     Is man EN heeft Snor breder dan 10 cm OF is vrouw EN draagt rood krulhaar langer dan 20 cm.
-#     Let op: Je hoeft alleen een man naar zijn snorlengte te vragen en alleen een vrouw naar haar krulhaarlengte.
-#     Is langer dan 150 cm
-#     Is zwaarder dan 90 kg
-#     Heeft Certificaat “Overleven met gevaarlijk personeel”
-#
-# De vragen met getallen mogen de kritieke grens (zoals 4 jaar, of 150 cm) van een criterium niet verklappen.  Dus je vraagt bijvoorbeeld:
-#
-#     'Wat is uw lichaamsgewicht?’ in plaats van 'Weegt  u zwaarder dan 90kg? J/N’
-#     ‘Hoeveel jaar praktijkervaring heeft u met dieren-dressuur?’ in plaats van 'Heeft u meer dan 4 jaar praktijkerbaring dieren-dressuur? J/N’
-#
-# Om de kandidaat te verwarren voeg je ook 4 vragen toe die er niet toe doen. Ga verder spelen met maffe criteria.
-
 input("""
 ######################################################################################################################################
 #                                                                                        Welkom bij de sollicitatieronde circusdirecteur voor circus HotelDeBotel.                                                                                                                             #
