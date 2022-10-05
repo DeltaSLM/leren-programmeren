7d70fc6b73e51a19a9c84821a5ccd5d788c6cfbb
DeltaSLM <joshuaslui0203@gmail.com>
2022-10-05 12:03:30 +0200

    Blastoff

---
+++ Software_Developer/Leerroutes/Leren_Programmeren/Module_03/deel01/Opdracht_2/blastoff.py
@@ -0,0 +1,5 @@
+for x in range(30,  0, -1):
+    print(f"Launching in {x}...")
+
+print("Falcon 9 launching now...")
+print("Falcon 9 has launched successfully and is now in orbit, congratulations.")
