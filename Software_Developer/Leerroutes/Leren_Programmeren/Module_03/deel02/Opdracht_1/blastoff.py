971c83f5a03b6880d0ed628492134db003911506
DeltaSLM <joshuaslui0203@gmail.com>
2022-10-11 14:14:11 +0200

    WHILE loop blastoff

---
+++ Software_Developer/Leerroutes/Leren_Programmeren/Module_03/deel02/Opdracht_1/blastoff.py
@@ -0,0 +1,8 @@
+lancering = 30
+
+while lancering != 0:
+    print("Launching in {}...".format(lancering))
+    lancering -= 1
+
+print("Falcon 9 launching now...")
+print("Falcon 9 launched successfully and is now in orbit. Congratulations.")
