9c82175e8c9036c4254668804608df1c6230e5c8
DeltaSLM <joshuaslui0203@gmail.com>
2022-10-12 10:25:13 +0200

    calculationing

---
+++ Software_Developer/Leerroutes/Leren_Programmeren/Module_03/deel02/Opdracht_5/sum.py
@@ -0,0 +1,9 @@
+answer = 50
+addition = 51
+toprint = "50"
+while answer < 1000:
+    answer += addition #1
+    toprint += "+" + str(addition) # 2
+    print(toprint, "=", answer) # 3
+    addition += 1 # 4
+
