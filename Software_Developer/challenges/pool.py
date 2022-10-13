e3eb5bb57766bf14679860a67c09cc68a4dd84e6
DeltaSLM <joshuaslui0203@gmail.com>
2022-10-13 09:43:49 +0200

    Finish pool

--- Software_Developer/challenges/pool.py
+++ Software_Developer/challenges/pool.py
@@ -8,6 +8,7 @@ try:
     hoogte = float(input("Wat is de hoogte van het zwembad?").replace(",", "."))
 except ValueError:
     print("Geef alstublieft een nummer door.")
+    exit()
 
 
 diepte = lengte*breedte*hoogte
@@ -18,12 +19,14 @@ afvoeren = diepte*32.50
 voorrij = 250+(60*2.05) if  diepte > 20 else 100+(60*2.05)
 
 m2 = lengte*breedte
+betontegel = m2*200+m2*20 if diepte >= 20 else m2*250+m2*25
 
 print(f"""
 Offerte voor een zwembad van 8 bij 3 bij 1,5 meter (inhoud {diepte:.2f} m2)
-Uitgraven:            {uitgraven:.2f}
-Afvoeren grond: {afvoeren:.2f}
-Voorrijkosten:     {voorrij}
-Totaal:                  {uitgraven+afvoeren+voorrij:.2f}
+Uitgraven:                     {uitgraven:.2f}
+Afvoeren grond:          {afvoeren:.2f}
+Voorrijkosten:              {voorrij}
+Beton + tegel ({m2}): {betontegel}
+Totaal:                           {uitgraven+afvoeren+voorrij+betontegel:.2f}
 
 """)
