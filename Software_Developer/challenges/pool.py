7639d849d4df89221c55e29948e6bb035d96d76b
DeltaSLM <joshuaslui0203@gmail.com>
2022-10-13 09:30:54 +0200

    step 2 (voorrijkosten)

--- Software_Developer/challenges/pool.py
+++ Software_Developer/challenges/pool.py
@@ -3,11 +3,13 @@ print(diepte)
 
 uitgraven = diepte*25
 afvoeren = diepte*32.50
+voorrij = 250+(60*2.05)
 
 print(f"""
 Offerte voor een zwembad van 8 bij 3 bij 1,5 meter (inhoud {diepte:.2f} m2)
-Uitgraven:             {uitgraven:.2f}
+Uitgraven:            {uitgraven:.2f}
 Afvoeren grond: {afvoeren:.2f}
-Totaal:                    {uitgraven+afvoeren:.2f}
+Voorrijkosten:     {voorrij}
+Totaal:                  {uitgraven+afvoeren+voorrij:.2f}
 
 """)
