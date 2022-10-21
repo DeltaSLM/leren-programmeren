c23473ef80d071404f7b6ecce4ab53027d3421de
DeltaSLM <joshuaslui0203@gmail.com>
2022-10-21 09:55:53 +0200

    fix excersize 11

--- Software_Developer/Leerroutes/Leren_Programmeren/Module_03/deel02/Opdracht_7_or_8/Forever_Young/robotarm-python-2021-main/oefening11.py
+++ Software_Developer/Leerroutes/Leren_Programmeren/Module_03/deel02/Opdracht_7_or_8/Forever_Young/robotarm-python-2021-main/oefening11.py
@@ -5,15 +5,21 @@ robotArm = RobotArm('exercise 11')
 # Jouw python instructies zet je vanaf hier::
 
 for i in range(8):
+    robotArm.moveRight()
+
+for i in range(9):
     robotArm.grab()
     color = robotArm.scan()
     if color == "white":
         robotArm.moveRight()
         robotArm.drop()
-        robotArm.moveRight()
+        if i < 8:
+            robotArm.moveLeft()
+            robotArm.moveLeft()
     else:
         robotArm.drop()
-        robotArm.moveRight()
+        if i < 8:
+            robotArm.moveLeft()
 
 # Na jouw code wachten tot het sluiten van de window:
 robotArm.wait()
