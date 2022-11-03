d574d5dae83c90d8d874abbe6a0f2c41579d03e1
DeltaSLM <joshuaslui0203@gmail.com>
2022-11-03 12:59:50 +0100

    Het getalraaadspell

---
+++ Software_Developer/Leerroutes/Leren_Programmeren/Module_03/Proef_A/guessing.py
@@ -0,0 +1,73 @@
+import random
+
+score = 0
+rounds = 1
+
+def guesser(question):
+    #print("number ", number)  # uncomment this for debugging
+    global guess
+    while not (guess := input(question)).isdigit():
+        if guess == "stop":
+            print("Exiting game with a score of {}, we're sad to see you go.".format(score))
+            exit()
+        print("--- You need to provide us with a number between 1 and 1000 or \"stop\" to stop ---")
+        pass
+    #print(guess)
+
+    if guess == str(number):
+        return True
+
+    else:
+        return False
+
+for x in range(20):
+    if x > 0:
+        while not (anotherRound := input("Do you want to play another round? Y/N").lower() == "y", "n"):
+            pass
+        if anotherRound == "y":
+            pass
+        elif anotherRound == "n":
+            break
+
+    number = random.randint(1, 1000)  # The number that has to be guessed
+
+    if guesser("Guess a number between 1 to 1000") == True:
+        score += 1
+        rounds += 1
+        if x != 20:
+            print("You guessed correctly! Moving onto round {} with score {}".format(rounds, score))
+
+    else:
+        guesses = 10
+        for x in range(10):
+            if x == 10:
+                print("You have failed to guess correctly, moving onto round {} with score {}".format(rounds, score))
+                break
+            #if number - guess > 50:
+                #print("Guess higher")
+
+            if guesser("Please guess again. You have {} guesses left.".format(guesses)) == True:
+                score += 1
+                rounds += 1
+                print("You guessed correctly! Moving onto round {} with score {}".format(rounds, score))
+                break
+
+            else:
+                sum = number - int(guess)
+
+                if sum > 50:
+                    print("Guess higher")
+                elif sum < -50:
+                    print("Guess lower")
+                elif sum > 20 and sum < 50 or sum < -20 and sum > -50:
+                    print("Getting warm...")
+                elif sum < 20 or sum > -20:
+                    print("Getting very warm...")
+
+                guesses -= 1
+        rounds += 1
+
+print("Congratulations! You have finished the game with a score of {}".format(score))
+
+
+
