9da7024b30f58f9dca00784b7ef8d161f173a8f0
DeltaSLM <joshuaslui0203@gmail.com>
2022-11-04 12:41:34 +0100

    bugged guessing game

--- Software_Developer/Leerroutes/Leren_Programmeren/Module_03/Proef_A/guessing.py
+++ Software_Developer/Leerroutes/Leren_Programmeren/Module_03/Proef_A/guessing.py
@@ -7,11 +7,11 @@ def guesser(question):
     #print("number ", number)  # uncomment this for debugging
     global guess
     while not (guess := input(question)).isdigit():
+        print("Function")
         if guess == "stop":
             print("Exiting game with a score of {}, we're sad to see you go.".format(score))
             exit()
         print("--- You need to provide us with a number between 1 and 1000 or \"stop\" to stop ---")
-        pass
     #print(guess)
 
     if guess == str(number):
@@ -21,8 +21,10 @@ def guesser(question):
         return False
 
 for x in range(20):
-    if x > 0:
+    if x != 0:
         while not (anotherRound := input("Do you want to play another round? Y/N").lower() == "y", "n"):
+            print(anotherRound := input("Do you want to play another round? Y/N").lower() == "y", "n")
+            print(" ask round")
             pass
         if anotherRound == "y":
             pass
@@ -32,12 +34,14 @@ for x in range(20):
     number = random.randint(1, 1000)  # The number that has to be guessed
 
     if guesser("Guess a number between 1 to 1000") == True:
+        print(" the round guess")
         score += 1
         rounds += 1
         if x != 20:
             print("You guessed correctly! Moving onto round {} with score {}".format(rounds, score))
 
     else:
+        print(" the failed guess")
         guesses = 10
         for x in range(10):
             if x == 10:
@@ -47,12 +51,14 @@ for x in range(20):
                 #print("Guess higher")
 
             if guesser("Please guess again. You have {} guesses left.".format(guesses)) == True:
+                print("the failed but now correct guess")
                 score += 1
                 rounds += 1
                 print("You guessed correctly! Moving onto round {} with score {}".format(rounds, score))
                 break
 
             else:
+                print(" the failed failed guess")
                 sum = number - int(guess)
 
                 if sum > 50:
@@ -68,6 +74,3 @@ for x in range(20):
         rounds += 1
 
 print("Congratulations! You have finished the game with a score of {}".format(score))
-
-
-
