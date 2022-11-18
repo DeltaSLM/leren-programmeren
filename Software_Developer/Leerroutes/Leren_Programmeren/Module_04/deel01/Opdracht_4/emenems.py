a7a3a20ee774ff3b8d759f5fb2002f7fda557fca
DeltaSLM <joshuaslui0203@gmail.com>
2022-11-18 08:54:41 +0100

    m&ms

--- Software_Developer/Leerroutes/Leren_Programmeren/Module_04/deel01/Opdracht_4/emenems.py
+++ Software_Developer/Leerroutes/Leren_Programmeren/Module_04/deel01/Opdracht_4/emenems.py
@@ -1,16 +1,18 @@
 import random
 
-MnMcolors = ['red', 'green', 'yellow', 'pink', 'blue']
+MnMcolors = ['red', 'green', 'geel', 'pink', 'blue']
 randomized = random.sample(MnMcolors, random.randint(1, 5))
 
-def decomposition(i):
-      # need help here
-      while i > 0:
-        n = random.randint(1, i)
-        yield n
-        i -= n
 
-rand_numbers = list(decomposition(200))
+def decomposition(i, x):
+    # need help here
+    ran = [random.randint(1, i) for _ in range(x)]
+    res = [ran[y] * i / sum(ran) for y in range(x)]
+    return map(round, res)
+
+
+rand_numbers = list(decomposition(200, len(randomized)))
+print(rand_numbers)
 thedict = {}
 for num, color in enumerate(randomized):
     thedict[color] = rand_numbers[num]
