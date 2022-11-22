0ebbfcbcb73f4817240ee02bed57fd67e5d41066
DeltaSLM <joshuaslui0203@gmail.com>
2022-11-22 10:11:25 +0100

    print random fruits

---
+++ Software_Developer/Leerroutes/Leren_Programmeren/Module_04/deel02/Opdracht_4/Opdracht04.py
@@ -0,0 +1,11 @@
+import random
+
+from fruitmand import fruitmand
+
+while not (amount := input("How much fruit would you like?")).isdigit():
+    print("NUMBER!!!!!")
+
+amount = int(amount)
+
+for x in range(amount):
+    print(fruitmand[random.randint(0,6)]['name'])
