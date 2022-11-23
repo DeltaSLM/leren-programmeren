525c9c71e72883c0e696c282f3ac5e39d9ca3dd6
DeltaSLM <joshuaslui0203@gmail.com>
2022-11-23 23:43:24 +0100

    adjust o.12 to work with modified fruitmand

--- Software_Developer/Leerroutes/Leren_Programmeren/Module_04/deel02/Opdracht_12/opdracht12.py
+++ Software_Developer/Leerroutes/Leren_Programmeren/Module_04/deel02/Opdracht_12/opdracht12.py
@@ -1,12 +1,13 @@
 from fruitmand import fruitmand
 
-translated = {"yellow": "gele", "green": "groene", "orange": "oranje", "red": "rode", "brown": "bruine"}
+translated = {"yellow": "gele", "green": "groene", "orange": "oranje", "red": "rode", "brown": "bruine", "pink": "roze", "purple": "paarse", "black": "zwarte"}
 length = []
+lengthnr = []
 
 for x in fruitmand:
-    length.append(x['name'])
+    lengthnr.append(len(x['name']))
 
-index = length.index(max(length))
+index = lengthnr.index(max(lengthnr))
 longestFruit = fruitmand[index]
 
 print(f"De {longestFruit['name']} ({len(longestFruit['name'])} letters) heeft een {translated[longestFruit['color']]} kleur en een gewicht van {longestFruit['weight'] / 1000}KG")
