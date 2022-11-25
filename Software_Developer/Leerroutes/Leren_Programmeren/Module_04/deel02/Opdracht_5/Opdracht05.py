876916a2b006d1c3e123a8ac9dae2519f7a19b22
DeltaSLM <joshuaslui0203@gmail.com>
2022-11-25 16:05:04 +0100

    alternative possibiltiy

--- Software_Developer/Leerroutes/Leren_Programmeren/Module_04/deel02/Opdracht_5/Opdracht05.py
+++ Software_Developer/Leerroutes/Leren_Programmeren/Module_04/deel02/Opdracht_5/Opdracht05.py
@@ -1,5 +1,10 @@
 from fruitmand import fruitmand
 
 fruitmand = reversed(fruitmand)
-for x in fruitmand:
-    print(x['name'])
+#for x in fruitmand:
+    #print(x['name'])
+
+# Andere mogelijkheid
+for x in range(len(fruitmand)-1,-1,-1):
+    # range: start, eind, hoeveelheid
+    print(fruitmand[x]['name'])
